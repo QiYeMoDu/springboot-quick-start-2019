5405d11e482490a1fad15c2e161233302bfa28da
dahai <17729720015@139.com>
2019-12-21 10:29:54 +0800

    1.提交项目的测试部分

---
+++ src/test/java/com/qiyemodu/springbootquickstart2019/SpringbootQuickStart2019ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiyemodu.springbootquickstart2019;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootQuickStart2019ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
